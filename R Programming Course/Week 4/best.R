258da4f958ac8ffcaa7314414759624f488c9957
alvaro_tendula <alvaro_tendula@live.com>
2019-02-16 01:50:03 +0100

    final submission
    implementation of rankall.R and rankhospital.R, as well as some changes to best.R

--- R Programming Course/Week 4/best.R
+++ R Programming Course/Week 4/best.R
@@ -15,11 +15,15 @@ best <- function(state, outcome){
   
   outcome_table <- read.csv("outcome-of-care-measures.csv")
   
-  outcome_pick
   # creates a dataframe with only the State Name, Hospital Name and 
   #chosen outcome collums
   
-  raw_table <- outcome_table[c(2,7, as.numeric(outcome_pick))]
+  raw_table <- outcome_table[c(2,7, outcome_pick)]
+  
+  #removes any data containing NA's , regular method was still leaving Not availables 
+ 
+  
+  
   
   #gets the list of unique states in the dataset
   states <- raw_table[["State"]]
@@ -36,35 +40,38 @@ best <- function(state, outcome){
     stop("invalid outcome")
     
   }else{
+    raw_data_na_to_clean <- raw_table[, 3] == "Not Available"
+    raw_table <- raw_table[!raw_data_na_to_clean, ]
+    
+    #forces conversion of Names and Outcome collumns
+    raw_table[,3] <- as.numeric(as.character(raw_table[,3]))
+    #forces conversion of collum names to characters
+    raw_table[,1] <- as.character(raw_table[,1])
     
     ##gets the data from only the selected state
     raw_to_clean <- complete.cases(as.numeric(raw_table[, 3]))
-    raw_data_cleaned <- raw_table[raw_to_clean, ]
-    
-    #removes any data containing NA's , regular method was still leaving Not availables 
-    raw_data_na_to_clean <- raw_data_cleaned[, 3] == "Not Available"
-    outcome_cleaned <- raw_data_cleaned[!raw_data_na_to_clean, ]
+    outcome_cleaned <- raw_table[raw_to_clean, ]
     
     #filters the data according to the picked state
     
-    states_to_remove <- (outcome_cleaned[["State"]] == as.character(state))
-    outcome_by_state_final <- outcome_cleaned[states_to_remove, ]
+    #states_to_remove <- (outcome_cleaned[["State"]] == as.character(state))
+    outcome_by_state_final <- subset(outcome_cleaned, State == state)
     
     #note : the collumn for the picked outcome will always be index 3
     
     #gets the lowest value of the outcome
-    min_outcome_value <- apply(outcome_by_state_final, 2, min )
     
     #extracts the hospitals with the lowest outcome
-    matched_hospitals <- outcome_by_state_final[,3]== min_outcome_value[3]
-    hospitals <- outcome_by_state_final[matched_hospitals,]
+    #matched_hospitals <- outcome_by_state_final[,3]== min_outcome_value[3]
+    #hospitals <- outcome_by_state_final[matched_hospitals,]
     
     #sorts the data in terms of the alfabetical order names
-    hospitals_sorted <- hospitals[order(hospitals[,1]), ]
+    hospitals_sorted <- outcome_by_state_final[order(outcome_by_state_final[,3], outcome_by_state_final[,1] ), ]
     
+    hospitals_sorted [1,1]
     
     #extracts the hospital name
-    as.character(hospitals_sorted[1,1])
+    #as.character(hospitals_sorted[1,1])
     
   }
   
--- R Programming Course/Week 4/rankall.R
+++ R Programming Course/Week 4/rankall.R
@@ -1,7 +1,8 @@
-rankhospital <- function(state, outcome, num = "best") {
+rankall <- function(outcome, num = "best"){
   
   outcome_pick <- NULL
   #selects the right collum index according to the chosen outcome
+  hospital_name <- NULL
   
   if (outcome == "heart attack"){
     outcome_pick <- 11
@@ -21,52 +22,46 @@ rankhospital <- function(state, outcome, num = "best") {
   
   raw_table <- outcome_table[c(2,7, as.numeric(outcome_pick))]
   
+  raw_data_na_to_clean <- raw_table[, 3] == "Not Available"
+  raw_table <- raw_table[!raw_data_na_to_clean, ]
+  
+  raw_table[,3] <- as.numeric(as.character(raw_table[,3]))
+  #forces conversion of collum names to characters
+  raw_table[,1] <- as.character(raw_table[,1])
+  
+  
+  #creates the dataframe for holding the result data
+  
+  out_df <- data.frame(hospital = character(), state = character())
+  
   #gets the list of unique states in the dataset
   states <- raw_table[["State"]]
-  list_states <- unique(states) 
+  list_states <- unique(states)
+
+  ##Cleans the data and removes any missing rows with the outcome information missing
+  raw_to_clean <- complete.cases(as.numeric(raw_table[, 3]))
+  outcome_data_cleaned <- raw_table[raw_to_clean, ]
   
-  #checks to see if the given state exists
-  stateExists <- state %in% list_states # checks to see if the state is valid
+  #removes any data containing NA's , regular method was still leaving Not availables 
   
-  #checks to see if the given state and outcome are valid
-  if(stateExists==FALSE){
-    stop("invalid state")
+  #note : the collumn for the picked outcome will always be index 3
+  
+  for (state in list_states){
+    #filters the table for each state
     
-  }else if (outcome_pick == 0){
-    stop("invalid outcome")
-    
-  }else{
-    #checks the value of the num argument
-    ##gets the data from only the selected state
-    raw_to_clean <- complete.cases(as.numeric(raw_table[, 3]))
-    raw_data_cleaned <- raw_table[raw_to_clean, ]
-    
-    #removes any data containing NA's , regular method was still leaving Not availables 
-    raw_data_na_to_clean <- raw_data_cleaned[, 3] == "Not Available"
-    outcome_cleaned <- raw_data_cleaned[!raw_data_na_to_clean, ]
-    
-    #filters the data according to the picked state
-    
-    states_to_remove <- (outcome_cleaned[["State"]] == as.character(state))
-    outcome_by_state_final <- outcome_cleaned[states_to_remove, ]
-    
-    #note : the collumn for the picked outcome will always be index 3
-    
-    #forces conversion of collumn 3 to numeric
-    outcome_by_state_final[,3] <- as.numeric(as.character(outcome_by_state_final[,3]))
-    #forces conversion of collum names to characters
-    outcome_by_state_final[,1] <- as.character(outcome_by_state_final[,1])
+    outcome_by_state_final <- subset(outcome_data_cleaned, State == state) 
     
     if (num == "best"){
       
       #sorts the data in terms of the outcome in ascending and selects the very first item in the table
       hospitals_sorted <- outcome_by_state_final[order(outcome_by_state_final[,3],outcome_by_state_final[,1]), ]
-      as.character(hospitals_sorted[1,1])
+      hospital_name <- as.character(hospitals_sorted[1,1])
       
     }else if (num == "worst"){
       #sorts the data in a descending way and selects the very first item in the table
       hospitals_sorted <- outcome_by_state_final[order(-outcome_by_state_final[,3],outcome_by_state_final[,1]), ]
-      as.character(hospitals_sorted[1,1])
+      hospital_name <- as.character(hospitals_sorted[1,1])
+      ### check error here
       
     }else if(is.numeric(num)& (num>0)){
       #sorts the data in an ascending manner in terms of the outcome and the hospital name, and selects the NUM item in the table
@@ -74,13 +69,19 @@ rankhospital <- function(state, outcome, num = "best") {
       
       #ensures that a result is only returned when the num argument is less or equal than the number of rows
       if(nrow(hospitals_sorted)>=num){
-        as.character(hospitals_sorted[num,1])
+        hospital_name <- as.character(hospitals_sorted[num,1])
         #hospitals_sorted
       }else{
-        NA
+        hospital_name <- NA
       }
       
     }
+    
+    into_df <- data.frame(hospital = hospital_name, state = state)
+    out_df <- rbind(out_df, into_df)
+    
   }
+  
+  out_df
   
 }
